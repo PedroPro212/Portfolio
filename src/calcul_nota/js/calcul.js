4abe0223baf9a1855860a5584f2949d1853d1034
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2024-05-30 15:28:31 -0300

    edit
    Modificação

--- src/calcul_nota/js/calcul.js
+++ src/calcul_nota/js/calcul.js
@@ -4,19 +4,36 @@ function calcularMedia(b1, b2){
 }
 
 function calcularFaltante(b1){
-    var faltante =  b1 - 70
+    var media = 70 * 5
+    media - (b1 * 2)
+    var faltante = media / 3
     return faltante
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     
-    var btn = document.querySelector('#btnCalcular')
-    var lbl = document.getElementById('lblResult')
+    var chc = document.getElementById('chcN2')
 
-    btn.addEventListener('click', () => {
+    chc.addEventListener('change', function(){
         var b1 = parseFloat(document.getElementById('txtB1').value)
-        var b2 = parseFloat(document.getElementById('txtB2').value)   
+        var b2 = document.getElementById('txtB2')
+        var lbl = document.getElementById('lblResult')
+
+        if(this.checked){
+            b2.style.display = 'none'
+            lbl.textContent = calcularFaltante(b1)
+        }else{
+            b2.style.display = 'block'
+
+            var btn = document.querySelector('#btnCalcular')
+            var lbl = document.getElementById('lblResult')
         
-        lbl.textContent = calcularMedia(b1, b2)
+            btn.addEventListener('click', () => {
+                var b1 = parseFloat(document.getElementById('txtB1').value)
+                var b2 = parseFloat(document.getElementById('txtB2').value)   
+        
+                lbl.textContent = calcularMedia(b1, b2)
+            })
+        }
     })
 })
